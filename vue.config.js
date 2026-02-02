e219720919564ee9345c170836b88a03866632b7
Akalinait <why202318@outlook.com>
2026-02-02 16:32:21 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-      ? '/guns-site/'  // 对应你的 Gitee 仓库名
+      ? '/sjz/'  // 对应你的 Gitee 仓库名
       : '/'
 })
